dd8ecc49e664530129adc71e7f9a1259984c0106
JzaaImp <91274153+JzaaImp@users.noreply.github.com>
2021-09-23 23:45:38 +0530

    test2

---
+++ connector.js
@@ -0,0 +1,125 @@
+var a = document.getElementById("arsh");
+a.addEventListener(
+  "touchstart",
+  (event) => {
+    window.location.href = "./letterindex.html";
+  },
+  false
+);
+
+var b = document.getElementById("aashima");
+b.addEventListener(
+  "touchstart",
+  (event) => {
+    window.location.href = "./letterindex1.html";
+  },
+  false
+);
+
+var c = document.getElementById("avadesh");
+c.addEventListener(
+  "touchstart",
+  (event) => {
+    window.location.href = "./letterindex2.html";
+  },
+  false
+);
+
+var d = document.getElementById("bitu");
+d.addEventListener(
+  "touchstart",
+  (event) => {
+    window.location.href = "./letterindex3.html";
+  },
+  false
+);
+
+var e = document.getElementById("happy");
+e.addEventListener(
+  "touchstart",
+  (event) => {
+    window.location.href = "./letterindex4.html";
+  },
+  false
+);
+
+var f = document.getElementById("himani");
+f.addEventListener(
+  "touchstart",
+  (event) => {
+    window.location.href = "./letterindex5.html";
+  },
+  false
+);
+
+var g = document.getElementById("kunal");
+g.addEventListener(
+  "touchstart",
+  (event) => {
+    window.location.href = "./letterindex6.html";
+  },
+  false
+);
+
+var h = document.getElementById("palak");
+h.addEventListener(
+  "touchstart",
+  (event) => {
+    window.location.href = "./letterindex8.html";
+  },
+  false
+);
+
+var i = document.getElementById("parth");
+i.addEventListener(
+  "touchstart",
+  (event) => {
+    window.location.href = "./letterindex9.html";
+  },
+  false
+);
+
+var j = document.getElementById("rohit");
+j.addEventListener(
+  "touchstart",
+  (event) => {
+    window.location.href = "./letterindex10.html";
+  },
+  false
+);
+
+var k = document.getElementById("sanya");
+k.addEventListener(
+  "touchstart",
+  (event) => {
+    window.location.href = "./letterindex11.html";
+  },
+  false
+);
+
+var l = document.getElementById("shweta");
+l.addEventListener(
+  "touchstart",
+  (event) => {
+    window.location.href = "./letterindex12.html";
+  },
+  false
+);
+
+var m = document.getElementById("tammana");
+m.addEventListener(
+  "touchstart",
+  (event) => {
+    window.location.href = "./letterindex13.html";
+  },
+  false
+);
+
+var n = document.getElementById("vaibhav");
+n.addEventListener(
+  "touchstart",
+  (event) => {
+    window.location.href = "./letterindex7.html";
+  },
+  false
+);
